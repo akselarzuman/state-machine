8a91c45312ffe01ecaf82d0df3547dfa3a16bf2e
Aksel Arzuman <akselarzuman@gmail.com>
2017-11-28 22:09:44 +0000

    Bug fix

--- StateMachine/Constant.cs
+++ StateMachine/Constant.cs
@@ -1,10 +1,10 @@
-﻿using StateMachine.Entities;
+﻿using StateMachine.Entities.Base;
 using System.Collections.Generic;
 
 namespace StateMachine
 {
     public class Constant
     {
-        public static List<State> States { get; set; }
+        public static List<BaseState> States { get; set; }
     }
 }
